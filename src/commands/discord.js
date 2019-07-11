aaa212b44c92b9af3782e3f9c01da9dbf29d7853
BlobbyDiscord <44066266+BlobbyDiscord@users.noreply.github.com>
2019-07-11 16:06:26 +0100

    Update discord.js

--- src/commands/discord.js
+++ src/commands/discord.js
@@ -5,15 +5,14 @@ async function invite(message) {
     try {
             const embed = {
                     author: {
-                        name: 'FossCord',
-                        url: 'https://fosscord.github.io/',
-                        icon_url: 'https://cdn.discordapp.com/icons/485768997607047179/d15af66a73043d2f6080edab0c05eb75.webp'
+                        name: 'FlyingMemes',
+                        url: 'https://flowradio.ml',
                     },
-                    description: turndownService.turndown(`<a href="https://discord.gg/7q5VcC6">Join support server</a>`),
+                    description: turndownService.turndown(`<a href="https://discord.gg/BY5BPnR">Join support server</a>`),
                     color: 16729344,
                     footer: {
                     icon_url: 'https://hackbrightacademy.com/content/uploads/2018/08/Reddit-logo.png',
-                    text: 'Reddit Bot by SerekKiri & MiXerek',
+                    text: 'Reddit Bot by IcyzDevelopment',
                 }
             }
 
